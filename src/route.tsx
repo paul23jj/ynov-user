0542225df3f8f40ad1cf4703ae25c1a065414e10
tom150407 <tomlaloeve@gmail.com>
2026-09-23 13:59:52 +0200

    connexion prsk fini

--- src/route.tsx
+++ src/route.tsx
@@ -1,22 +1,14 @@
 import { Outlet } from "react-router";
-<<<<<<< HEAD
-
-=======
 import { Navigate } from "react-router-dom";
->>>>>>> spike
 import App from "./App.tsx";
-import Favoris from "./pages/favoris.tsx";
+import Favoris from "./pages/Favoris.tsx";
 import Login from "./pages/Login.tsx";
 import Header from "./pages/Navbar.tsx";
 import NotFound from "./pages/NotFound.tsx";
-<<<<<<< HEAD
-
-=======
 import Profile from "./pages/Profile.tsx";
 import RecipeDetail from "./pages/RecipeDetail.tsx";
 import User from "./pages/User.tsx";
 import UserList from "./pages/UserList.tsx";
->>>>>>> spike
 import GuestRoute from "./routes/GuestRoute.tsx";
 import PrivateRoute from "./routes/PrivateRoute.tsx";
 
@@ -28,13 +20,20 @@ const Layout = () => (
 );
 
 const routes = [
-<<<<<<< HEAD
   {
     element: <Layout />,
     children: [
       {
         path: "/",
+        element: <Navigate to="/recipes" replace />,
+      },
+      {
+        path: "/recipes",
         element: <App />,
+      },
+      {
+        path: "/favoris",
+        element: <Favoris />,
       },
       {
         path: "/userList",
@@ -74,61 +73,6 @@ const routes = [
       },
     ],
   },
-=======
-    {
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <Navigate to="/recipes" replace />,
-            },
-            {
-                path: "/recipes",
-                element: <App />,
-            },
-            {
-                path: "/favoris",
-                element: <Favoris />,
-            },
-            {
-                path: "/userList",
-                element: <UserList />,
-            },
-            {
-                path: "/user/:id",
-                element: <User />,
-            },
-            {
-                path: "/recipe/:id",
-                element: <RecipeDetail />,
-            },
-            {
-                path: "/login",
-                element: (
-                    <GuestRoute>
-                        <Login />
-                    </GuestRoute>
-                ),
-            },
-            {
-                path: "/login",
-                element: (
-                    <PrivateRoute>
-                        <Login />
-                    </PrivateRoute>
-                ),
-            },
-            {
-                path: "/profile",
-                element: <Profile />,
-            },
-            {
-                path: "*",
-                element: <NotFound />,
-            },
-        ],
-    },
->>>>>>> spike
 ];
 
 export default routes;
