c2889a75e93a9b7b5c2f36c06015c8485abd32b4
tom150407 <tomlaloeve@gmail.com>
2026-09-23 13:40:10 +0200

    connexion prsk fini

--- src/route.tsx
+++ src/route.tsx
@@ -11,13 +11,8 @@ import NotFound from "./pages/NotFound.tsx";
 
 import GuestRoute from "./routes/GuestRoute.tsx";
 import PrivateRoute from "./routes/PrivateRoute.tsx";
-import PrivateRoute from "./routes/PrivateRoute.tsx";
 
 const Layout = () => (
-  <>
-    <Header />
-    <Outlet />
-  </>
   <>
     <Header />
     <Outlet />
@@ -73,4 +68,3 @@ const routes = [
 ];
 
 export default routes;
-
